6dde2528b9e7750c663f9b805ced8c7c4a3b21b8
Anton Fisher <a.fschr@gmail.com>
2018-10-04 14:15:54 -0700

    NEX-18622 - add defaultDataIP and defaultDataset startup options, readme

--- README.md
+++ README.md
@@ -8,18 +8,26 @@ NexentaStor CSI driver for Kubernetes.
 make
 ```
 
+## Run
+```bash
+make && ./bin/nexentastor-csi-plugin --rest-ip="https://10.3.199.253:8443,https://10.3.199.252:8443" --username="admin" --password="Nexenta@1"
+```
+
 ## Tests
 
-Run all tests:
 ```bash
+# run all tests
+make test
+# or
+make test | grep --color 'FAIL\|$'
+
+# with options
 go test ./tests/**             # run all
 go test ./tests/** -v          # more output
 go test ./tests/** -v -count 1 # disable cache
-```
 
-Run NexentaStor API tests with options:
-```bash
-go test ./tests/nexentastor -v -count 1 \
+# NexentaStor provider test options
+go test ./tests/ns_provider -v -count 1 \
     --address="https://10.3.199.254:8443" \
     --username="admin" \
     --password="pass" \
--- src/main.go
+++ src/main.go
@@ -16,12 +16,14 @@ const (
 
 func main() {
 	var (
-		nodeID   = flag.String("nodeid", "", "Kubernetes node ID")
-		endpoint = flag.String("endpoint", defaultEndpoint, "CSI endpoint")
-		address  = flag.String("address", "", "NexentaStor API address [schema://host:port,...]")
-		username = flag.String("username", "", "overwrite NexentaStor API username from config")
-		password = flag.String("password", "", "overwrite NexentaStor API password from config")
-		version  = flag.Bool("version", false, "Print driver version")
+		nodeID         = flag.String("nodeid", "", "Kubernetes node ID")
+		endpoint       = flag.String("endpoint", defaultEndpoint, "CSI endpoint")
+		address        = flag.String("rest-ip", "", "NexentaStor API address(s) [schema://host:port,...]")
+		username       = flag.String("username", "", "NexentaStor API username")
+		password       = flag.String("password", "", "NexentaStor API password")
+		defaultDataset = flag.String("default-dataset", "", "default dataset to create filesystems on")
+		defaultDataIP  = flag.String("default-data-ip", "", "default data IP for sharing filesystems")
+		version        = flag.Bool("version", false, "Print driver version")
 	)
 
 	flag.Parse()
@@ -33,21 +35,21 @@ func main() {
 
 	if len(*address) == 0 {
 		fmt.Print(
-			"NexentaStor address is not set, use 'address' option in config file or CLI",
+			"NexentaStor REST API address is not set, use 'restIp' option in the secret",
 		)
 		os.Exit(1)
 	}
 
 	if len(*username) == 0 {
 		fmt.Print(
-			"NexentaStor username is not set, use 'username' option in config file or CLI",
+			"NexentaStor REST API username is not set, use 'username' option in the secret",
 		)
 		os.Exit(1)
 	}
 
 	if len(*password) == 0 {
 		fmt.Print(
-			"NexentaStor password is not set, use 'password' option in config file or CLI",
+			"NexentaStor REST API password is not set, use 'password' option in the secret",
 		)
 		os.Exit(1)
 	}
@@ -55,7 +57,6 @@ func main() {
 	// init logger
 	log := logrus.New().WithFields(logrus.Fields{
 		//"nodeId":    *nodeID,
-		//"address":   *address,
 		"cmp": "Main",
 	})
 
@@ -63,9 +64,11 @@ func main() {
 	log.Logger.SetLevel(logrus.DebugLevel)
 
 	log.Info("Start driver with:")
-	log.Infof("- CSI endpoint: '%v'\n", *endpoint)
-	log.Infof("- Node ID:      '%v'\n", *nodeID)
-	log.Infof("- NS address:   '%v'\n", *address)
+	log.Infof("- CSI endpoint:    '%v'\n", *endpoint)
+	log.Infof("- Node ID:         '%v'\n", *nodeID)
+	log.Infof("- NS address(s):   '%v'\n", *address)
+	log.Infof("- Default dataset: '%v'\n", *defaultDataset)
+	log.Infof("- Default data IP: '%v'\n", *defaultDataIP)
 
 	//TESTS
 

c26df720d4369f701259809c62fc467e0a0330bf
Anton Fisher <a.fschr@gmail.com>
2019-07-10 10:52:30 -0700

    chore: update logrus to v1.4.2

--- tests/deploy/scripts/combine-all-driver-logs.sh
+++ tests/deploy/scripts/combine-all-driver-logs.sh
@@ -6,5 +6,5 @@
 
 { \
     kubectl logs -f --all-containers=true nexentastor-csi-controller-0 & \
-    kubectl logs -f --all-containers=true $(kubectl get pods | awk '/nexentastor-csi-driver/ {print $1;exit}'); \
+    kubectl logs -f --all-containers=true $(kubectl get pods | awk '/nexentastor-csi-node/ {print $1;exit}'); \
 }
